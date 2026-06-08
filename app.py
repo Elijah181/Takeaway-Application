96419fa8c58ababe0c8bb235c4df9278cc8753a5
Elijah <moree@stu.otc.school.nz>
2026-06-08 15:16:47 +1200

    Menu Code finished

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    names = ["Elijah", "Rafael", "Jordan"]
-    return render_template("menu.html", names=names)
+    return render_template("menu.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
