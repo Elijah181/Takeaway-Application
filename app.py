140d6797ad1245404f0604a321884b57b410d2cb
Elijah <moree@stu.otc.school.nz>
2026-06-11 13:26:15 +1200

    made the code for accessing item page

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template("menu.html")
+def index():
+    return render_template('menu.html')
 
-if __name__ == "__main__":
+@app.route('/process-data', methods=['POST'])
+def submit_data():
+
+    received_item_id = request.form.get('item_id')
+    print(f"Received Item ID: {received_item_id}")
+    if received_item_id == "bacon_cheeseburger":
+        menu_description = "temporary text"
+    return render_template('item.html', description = menu_description)
+
+if __name__ == '__main__':
     app.run(debug=True)
